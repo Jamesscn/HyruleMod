b688911dfbb78c8e462cee35b98e6e10bd13f741
James Scoon <jamesscn@gmail.com>
2020-01-11 21:23:52 -0600

    Setup
    Setup is complete and now runs in Minecraft

---
+++ src/main/java/com/jamesscn/zeldaHyrule/HyruleMod.java
@@ -0,0 +1,7 @@
+package com.jamesscn.zeldaHyrule;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(HyruleMod.MOD_ID)
+public class HyruleMod {
+    public static final String MOD_ID = "zeldahyrule";
+}
